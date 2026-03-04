767ef0d7e26591180571986213bab067815a9b62
Zakari <zbautista@csus.edu>
2026-03-04 01:23:54 -0800

    Finished writing the EOF, skip empty, as well as strdup'd fields in parse lines

--- src/main.c
+++ src/main.c
@@ -15,4 +15,27 @@ int main(void){
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
     sigaction(SIGINT, &sa, NULL);
+
+    while (1) {
+        printf("myUnix> ");
+        fflush(stdout);
+        ssize_t n = getline(&line, &len, stdin);
+        if (n == -1) { //EOF (CRTL+D)
+            printf("\n");
+            break;
+        }
+        //skip empty
+        if (n == 1) continue;
+
+        command_t cmd;
+        parse_line(line, &cmd);
+        execute_command(&cmd);
+
+        //free strdup'd fields in parse_line 
+        for (int i=0; cmd.argv[i]; i++) free(cmd.argv[i]);
+        if (cmd.infile) free(cmd.infile);
+        if (cmd.oufile) free(cmd.outfile);
+    }
+    free(line);
+    return 0;
 }
